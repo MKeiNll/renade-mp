fb55a827dd2881301dd5cc1207add7a2b9b1b573
MKeiNll <levgirich@gmail.com>
2019-10-23 20:39:58 +0300

    add pictures of parents & fix their names

--- bridge/resources/renade/Model/Character/Appearance.cs
+++ bridge/resources/renade/Model/Character/Appearance.cs
@@ -9,13 +9,13 @@ namespace renade
     public enum Mother
     {
         Hannah = 21, Audrey = 22, Jasmine = 23, Giselle = 34, Amelia = 25,
-        Isabella = 26, Zoe = 27, Ava = 28, Camilla = 29, Violet = 30, Sophia = 31,
-        Eveline = 32, Nicole = 33, Ashley = 34, Grace = 35, Brianna = 36, Natalie = 37,
+        Isabella = 26, Zoe = 27, Ava = 28, Camila = 29, Violet = 30, Sophia = 31,
+        Evelyn = 32, Nicole = 33, Ashley = 34, Grace = 35, Brianna = 36, Natalie = 37,
         Olivia = 38, Elizabeth = 39, Charlotte = 40, Emma = 41, Misty = 45
     };
     public enum Father
     {
-        Benjamin = 0, Daniel = 1, Joshua = 2, Noah = 3, Andrew = 4, Joan = 5,
+        Benjamin = 0, Daniel = 1, Joshua = 2, Noah = 3, Andrew = 4, Juan = 5,
         Alex = 6, Isaac = 7, Evan = 8, Ethan = 9, Vincent = 10, Angel = 11, Diego = 12,
         Adrian = 13, Gabriel = 14, Michael = 15, Santiago = 16, Kevin = 17, Louis = 18,
         Samuel = 19, Anthony = 20, John = 42, Niko = 43, Claude = 44
